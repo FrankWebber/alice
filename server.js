9dd60efe7a73a7486aac0bc2a168bedf31ff4d43
FrankWebber <47332986+FrankWebber@users.noreply.github.com>
2024-08-02 23:24:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Servir arquivos estáticos da pasta 'web-build' (diretório de saída da compilação do Expo)
+// Servir arquivos estáticos da pasta 'web-build' (diretório de saída da exportação do Expo)
 app.use(express.static(path.join(__dirname, 'web-build')));
 
 app.get('*', (req, res) => {
